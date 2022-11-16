5f96856e0d272a6cbb74b01aee9d6de443cef8c6
andreiserbanrazvan <andreiserbanrazvan@gmail.com>
2022-11-16 10:27:21 +0200

    fixed formatting

--- main.lua
+++ main.lua
@@ -1,20 +1,18 @@
-
 function love.load()
-    bresenham = require 'bresenham' 
+    bresenham = require 'bresenham'
 end
-
 
 function love.draw()
     if love.mouse.isDown(1) then
         initial_mX, initial_mY = love.mouse.getPosition()
     end
-    if not(initial_mX == nil) and not(initial_mY == nil) then
-        mX,mY = love.mouse.getPosition()
+    if not (initial_mX == nil) and not (initial_mY == nil) then
+        mX, mY = love.mouse.getPosition()
         if mX == nil then mX = 0 end
         if mY == nil then mY = 0 end
-        bresenham.los(initial_mX,initial_mY,mX,mY, function(mX,mY)
-            
-            love.graphics.circle('fill',mX,mY, 10)
+        bresenham.los(initial_mX, initial_mY, mX, mY, function(mX, mY)
+
+            love.graphics.circle('fill', mX, mY, 10)
             return true
         end)
     end
